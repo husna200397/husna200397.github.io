7de0577648dcb514c48ce9a5c0ff55443258edae
husna200397 <norqurratuhusna97@gmail.com>
2021-04-02 20:27:13 +0800

    added check pass

--- event-handler/js/index.js
+++ event-handler/js/index.js
@@ -7,7 +7,16 @@ elNamaPengguna.addEventListener("keyup", function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
 });
 
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan");
+    let password_val = elKataLaluan.value;
+    if(password_val.length > 5){
+        alert("Password Check...OK!");
+    } else {
+        alert("Password too short!")
+    }
+}
 
-
-
+let elKataLaluan = document.getElementById("katalaluan");
+elKataLaluan.onblur = checkPassword;
     
