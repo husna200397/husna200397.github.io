75d8df453210dbe6ae398ca21823f2e7957b4e2f
husna200397 <norqurratuhusna97@gmail.com>
2021-04-05 21:32:10 +0800

    edited

--- video/js/index.js
+++ video/js/index.js
@@ -1,17 +1,20 @@
 let video = document.getElementById("myVideo");
 
 let playBtn = document.getElementById("playBtn");
-playBtn.addEventListener("click", function(){
+playBtn.addEventListener("click", function()
+{
     video.play();
 });
 
 let pauseBtn = document.getElementById("pauseBtn");
-pauseBtn.addEventListener("click",function(){
+pauseBtn.addEventListener("click",function()
+{
     video.pause();
 });
 
 let freezeBtn = document.getElementById("freezeBtn");
-freezeBtn.addEventListener("mousedown", function(){
+freezeBtn.addEventListener("mousedown", function()
+{
     video.pause();
 });
 freezeBtn.addEventListener("mouseup", function(){
@@ -23,7 +26,7 @@ seekBar.addEventListener("change", function(){
     let time = video.duration * (seekBar.value / 100);
     video.currentTime = time;
 });
-seekBar.addEventListener("timeupdate", function(){
+video.addEventListener("timeupdate", function(){
     let value = (100 / video.duration) * video.currentTime;
     seekBar.value = value;
 });
