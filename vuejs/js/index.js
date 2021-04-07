d2df77052d09545a2a32d1f4740b296cceb7a51b
husna200397 <norqurratuhusna97@gmail.com>
2021-04-07 21:36:43 +0800

    url

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -9,7 +9,7 @@ let app2 = new Vue({
     el: "#app2",
     data: {
         message: "Loaded on " + new Date().toLocaleDateString(),
-        linkurl: "https://bbc.in/2LAnJs8",
+        linkURL: "https://bbc.in/2LAnJs8",
         linkText: "Google is down!"
     }
 });
