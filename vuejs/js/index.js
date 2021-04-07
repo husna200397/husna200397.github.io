2b7470962aa8d7e099e1c8c2b1093d2d0edf864b
husna200397 <norqurratuhusna97@gmail.com>
2021-04-07 20:42:04 +0800

    8.41pm

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -4,3 +4,11 @@ let app = new VTTCue({
         message:"Hello Vue!"
     }
 });
+let app2 = new Vue({
+    el: "#app2",
+    data: {
+        message: "Loaded on " + new Date().toLocaleDateString()
+        linkUrl: "https://bbc.in/2LAnJs8",
+        linkText: "Google is down!"
+    }
+});
