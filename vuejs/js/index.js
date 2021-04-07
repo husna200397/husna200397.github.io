dfdd4d5d96e8036a0b727c1e38a68e6361773b94
husna200397 <norqurratuhusna97@gmail.com>
2021-04-07 21:01:48 +0800

    app4

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -4,11 +4,36 @@ let app = new Vue({
         message:"Hello Vue!"
     }
 });
+
 let app2 = new Vue({
     el: "#app2",
     data: {
         message: "Loaded on " + new Date().toLocaleDateString()
         linkUrl: "https://bbc.in/2LAnJs8",
         linkText: "Google is down!"
+    }
+});
+
+let app3 = new Vue({
+    el: "#app3",
+    data: {
+        showIt: false
+    }
+});
+
+let app4 = new Vue({
+    el: "#app4",
+    data: {
+        theList: [
+            {
+                text:"Learn HTML"
+            },
+            {
+                text:"Learn CSS"
+            },
+            {
+
+            }
+        ]
     }
 });
